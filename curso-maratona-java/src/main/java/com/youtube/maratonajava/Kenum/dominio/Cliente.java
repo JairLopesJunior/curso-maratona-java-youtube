ce6102b25fb0fec46ffecc6ff0e75a3b1069187c
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-13 23:30:49 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/Kenum/dominio/Cliente.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Kenum/dominio/Cliente.java
@@ -10,27 +10,12 @@ public class Cliente {
         this.tipoCliente = tipoCliente;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public TipoCliente getTipoCliente() {
-        return tipoCliente;
-    }
-
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente;
-    }
-
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
                 ", tipoCliente=" + tipoCliente +
+                ", tipoClienteInt=" + tipoCliente.getValor() +
                 '}';
     }
 }
--- curso-maratona-java/src/main/java/com/youtube/maratonajava/Kenum/dominio/TipoCliente.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Kenum/dominio/TipoCliente.java
@@ -2,6 +2,22 @@ package com.youtube.maratonajava.Kenum.dominio;
 
 public enum TipoCliente {
 
-    PESSOA_FISICA,
-    PESSOA_JURIDICA;
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int valor;
+    public String nomeRelatorio;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
