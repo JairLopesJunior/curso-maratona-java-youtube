c15bfb6d74011bba9f64c7d8ec8a05810f6b23f2
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-15 15:03:06 -0300

    84 - Orientação Objetos - Classes abstratas pt 01

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Lclassesabstratas/dominio/Main.java
@@ -0,0 +1,12 @@
+package com.youtube.maratonajava.Lclassesabstratas.dominio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 12000);
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
