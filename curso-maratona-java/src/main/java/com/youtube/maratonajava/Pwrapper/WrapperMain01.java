6be4559da3213187317f54314dd8638b5a0d983f
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-25 14:19:27 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Pwrapper/WrapperMain01.java
@@ -0,0 +1,26 @@
+package com.youtube.maratonajava.Pwrapper;
+
+public class WrapperMain01 {
+
+    public static void main(String[] args) {
+        // TIPOS PRIMITIVOS
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        // TIPOS WRAPPERS
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+    }
+}
