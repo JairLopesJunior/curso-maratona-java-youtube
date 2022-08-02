1a817eab99b6cc8ea3d1567326bb1c1300bfc0d5
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-01 23:28:09 -0300

    131 - Classes Utilitárias - Regex pt 01 - Pattern e Matcher

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Uregex/PatternMatcherMain01.java
@@ -0,0 +1,44 @@
+package com.youtube.maratonajava.Uregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherMain01 {
+
+    public static void main(String[] args) {
+
+        String regex = "ab";
+        String texto = "abaaba";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:  " + texto);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex " + regex);
+        System.out.println("Posições encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
