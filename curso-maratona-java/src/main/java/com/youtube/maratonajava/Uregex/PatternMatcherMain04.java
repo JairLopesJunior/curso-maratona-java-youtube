dd0f1a418a0e820071dea670d697b68898e4c481
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-02 23:24:22 -0300

    134 - Classes Utilitárias - Regex pt 04 - Pattern e Matcher - Quantificadores pt 01

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Uregex/PatternMatcherMain04.java
@@ -0,0 +1,58 @@
+package com.youtube.maratonajava.Uregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherMain04 {
+
+    public static void main(String[] args) {
+        // \d = Retorna todos os digitos
+        // \D = Retorna tudo o que não for digito
+        // \s = Retorna os espaços em branco (\t, \n, \f, \r)
+        // \S = Retorna todos os caracters excluindo os espaços em branco
+        // \w = Retorna todos os a-z, A-Z, digitos, _
+        // \W = Retorna tudo que não for incluso no \w
+        // []
+        // ? = Zero ou uma ocorrencia
+        // * = Zero ou mais ocorrencias
+        // + = Uma ou mais ocorrencias
+        // {n, m} = De n até m ocorrencias
+        // () = Agrupamento
+        // | = Ou
+        // $ = Representa o fim da linha
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:  " + texto);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex " + regex);
+        System.out.println("Posições encontradas");
+        while (matcher.find()) {
+            //System.out.print(matcher.start() + " " + matcher.group() + "\n");
+            System.out.print(matcher.group() + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
