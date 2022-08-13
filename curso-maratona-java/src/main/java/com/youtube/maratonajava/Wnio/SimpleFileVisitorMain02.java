67e22ef333967eaeeb1dc265ee6b734245d123fc
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-13 18:44:04 -0300

    155 - Classes Utilitárias - NIO pt 12 - SimpleFileVisitor pt 02

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Wnio/SimpleFileVisitorMain02.java
@@ -0,0 +1,40 @@
+package com.youtube.maratonajava.Wnio;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles02 extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        System.out.println("preVisitDirectory " + dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("postVisitDirectory " + dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorMain02 {
+
+    public static void main(String[] args) throws IOException {
+
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new ListAllFiles02());
+    }
+}
