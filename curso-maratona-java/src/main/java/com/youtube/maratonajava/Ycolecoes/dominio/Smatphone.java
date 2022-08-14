a699d00108d95f7ef6a735b34d26ad034d7ef086
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-14 18:10:13 -0300

    162 - Coleções pt 02 - equals pt 02

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/dominio/Smatphone.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/dominio/Smatphone.java
@@ -10,6 +10,21 @@ public class Smatphone {
         this.marca = marca;
     }
 
+    // Regras do Equals
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simétrico: Para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitividade: Para x, y, z diferentes de null, se x.equals(y) == true, logo x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // Para x diferente de null, x.equals(null) tem que retornar false.
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smatphone smatphone = (Smatphone) obj;
+        return serialNumber != null && serialNumber.equals(smatphone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
