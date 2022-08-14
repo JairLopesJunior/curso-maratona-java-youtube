1a6d65c25e2eef8598c53e2d8c00a29a565372b0
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-14 19:02:11 -0300

    164 - Coleções pt 04 - hashCode pt 02

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/dominio/Smatphone.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/dominio/Smatphone.java
@@ -25,6 +25,16 @@ public class Smatphone {
         return serialNumber != null && serialNumber.equals(smatphone.serialNumber);
     }
 
+    // Regras do HashCode
+    // Se x.equlas(y) == true, y.hashCode() == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashCode() != x.hashCode(), x.equals(y) deverá ser false
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
