6b53d944a994f7ade943bce165f4e0dcdcb2363b
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-14 22:35:59 -0300

    167 - Coleções pt 07 - List pt 02

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/list/ListMain01.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/list/ListMain01.java
@@ -10,11 +10,18 @@ public class ListMain01 {
         List nomes = new ArrayList(); // Até a versão 1.4 era feito desta forma
         nomes.add("Jair");
         nomes.add("DevDojo Academy");
-        nomes.add(1);
+        //nomes.add(1);
         /*for(Object nome: nomes) {
             System.out.println(nome);
         }*/
 
         List<String> nomess = new ArrayList<>(); // Forçando em tempo de compilação a lista ser de String (A partir da versão 1.5)
+        nomess.add("Jair"); // Adiciona na lista
+        nomess.add("João");
+        nomess.add("Carlos");
+        nomess.remove("Jair"); // Remove da lista
+        nomess.remove(0); // Remove da lista por indice
+        nomess.addAll(nomes); // Junta duas listas
+        nomess.forEach(System.out::println);
     }
 }
