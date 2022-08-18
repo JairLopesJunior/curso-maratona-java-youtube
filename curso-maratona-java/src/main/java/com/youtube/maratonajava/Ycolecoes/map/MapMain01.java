8b6d35d981ee376d79822e4d70c7c2169c0e0536
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-17 22:35:38 -0300

    178 - Coleções pt 18 - Map, HashMap, LinkedHashMap pt 01

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/map/MapMain01.java
@@ -0,0 +1,32 @@
+package com.youtube.maratonajava.Ycolecoes.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain01 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mouze", "mouse");
+        map.put("vc", "voce");
+        System.out.println(map);
+
+        for(String key: map.keySet()) {
+            System.out.println(key + " - " + map.get(key));
+        }
+
+        System.out.println("---------------------");
+
+        for(? value: map.values()) {
+            System.out.println(value);
+        }
+
+        System.out.println("---------------------");
+
+        for(Map.Entry<String, String> m: map.entrySet()) {
+            System.out.println("Key: " + m.getKey() + " : Value: " + m.getValue());
+        }
+    }
+}
