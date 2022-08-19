65c0c5b658e6fb554e1f37d15d766ef26c0b80ab
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-18 23:52:53 -0300

    181 - Coleções pt 21 - NavigableMap, TreeMap

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/Ycolecoes/map/NavigableMapMain01.java
@@ -0,0 +1,44 @@
+package com.youtube.maratonajava.Ycolecoes.map;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapMain01 {
+
+    public static void main(String[] args) {
+
+        NavigableMap<String, String> map = new TreeMap<>();
+        map.put("A", "Letra A");
+        map.put("D", "Letra D");
+        map.put("B", "Letra B");
+        map.put("C", "Letra C");
+        map.put("E", "Letra E");
+
+        for(Map.Entry<String, String> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        System.out.println();
+
+        System.out.println(map.headMap("B"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
