5a64c3de74490358496d158b45d997c357db885d
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-24 23:11:37 -0300

    199 - Method Reference pt 02 - Referência a métodos não estáticos

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZClambdas/dominio/MethodReferenceMain02.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZClambdas/dominio/MethodReferenceMain02.java
@@ -1,21 +1,21 @@
 package com.youtube.maratonajava.ZZClambdas.dominio;
 
+
 import com.youtube.maratonajava.ZZClambdas.dominio.service.AnimeComparators;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-// Reference to a static method
-public class MethodReferenceMain01 {
+// Reference to an instance method of a particular object
+public class MethodReferenceMain02 {
 
     public static void main(String[] args) {
-
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("Beserk", 43),
                 new Anime("One Piece", 900),
                 new Anime("Naruto", 500)));
-        //Collections.sort(animeList, (a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-        Collections.sort(animeList, AnimeComparators::compareByTitle);
+        AnimeComparators animeComparators = new AnimeComparators();
+
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
         System.out.println(animeList);
     }
 }
