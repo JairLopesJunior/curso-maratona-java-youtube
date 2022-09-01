7e2b42a368fdc61e5e4d37239d584c8aa854a762
unknown <jair.lopes@fatec.sp.gov.br>
2022-08-31 23:54:01 -0300

    213 - Streams pt 11 - Collectors pt 01 Summarizing

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZEstreams/StreamMain01.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZEstreams/StreamMain01.java
@@ -19,24 +19,27 @@ public class StreamMain01 {
             new LightNovel("Fullmetal Alchemist", 5.99),
             new LightNovel("Fullmetal Alchemist", 5.99),
             new LightNovel("Kumo desuga", 1.99),
-            new LightNovel("Monogatari", 4.00)
+            new LightNovel(null, 4.00)
     ));
 
     public static void main(String[] args) {
 
-        lightNovels.sort(Comparator.comparing(LightNovel::getTitle));
-        List<String> titles = new ArrayList<>();
+//        lightNovels.sort(Comparator.comparing(LightNovel::getTitle));
+//        List<String> titles = new ArrayList<>();
+//
+//        for(LightNovel lightNovel: lightNovels) {
+//            if(lightNovel.getPrice() <= 4) {
+//                titles.add(lightNovel.getTitle());
+//            }
+//            if(titles.size() >= 3) {
+//                break;
+//            }
+//        }
+//
+//        System.out.println(lightNovels);
+//        System.out.println(titles);
 
-        for(LightNovel lightNovel: lightNovels) {
-            if(lightNovel.getPrice() <= 4) {
-                titles.add(lightNovel.getTitle());
-            }
-            if(titles.size() >= 3) {
-                break;
-            }
-        }
-
+        lightNovels.sort(Comparator.comparing(LightNovel::getTitle, Comparator.nullsFirst(String::compareTo)));
         System.out.println(lightNovels);
-        System.out.println(titles);
     }
 }
