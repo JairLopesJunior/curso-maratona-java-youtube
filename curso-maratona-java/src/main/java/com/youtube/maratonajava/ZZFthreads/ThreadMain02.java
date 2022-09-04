03d99b5e0adda5abd5a1a8088fe80fd5f1e5033e
unknown <jair.lopes@fatec.sp.gov.br>
2022-09-04 16:25:35 -0300

    223 - Threads pt 04 - Yield e Join

---
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZFthreads/ThreadMain02.java
@@ -0,0 +1,43 @@
+package com.youtube.maratonajava.ZZFthreads;
+
+// Outra forma de criar Thread
+class ThreadExampleRunnable2 implements Runnable {
+
+    private final String c;
+
+    public ThreadExampleRunnable2(String c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 10; i++) {
+            System.out.print(c);
+            if(i % 3 == 0) {
+                System.out.println();
+            }
+            Thread.yield();
+        }
+    }
+}
+
+public class ThreadMain02 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Uma forma de criar Thread
+        //new Thread(() -> {}).start();
+
+        // Uma forma de criar Thread
+        //Runnable runnable = () -> {};
+
+        Thread t1 = new Thread(new ThreadExampleRunnable2("KA"), "T1");
+        Thread t2 = new Thread(new ThreadExampleRunnable2("ME"), "T2");
+
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+        t2.start();
+        t1.join();
+    }
+}
