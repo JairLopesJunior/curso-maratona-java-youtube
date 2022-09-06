0bdd1677df30c519eecec0db15f71e81c43f5993
unknown <jair.lopes@fatec.sp.gov.br>
2022-09-05 23:45:58 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

--- curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZFthreads/ThreadAccountMain01.java
+++ curso-maratona-java/src/main/java/com/youtube/maratonajava/ZZFthreads/ThreadAccountMain01.java
@@ -4,7 +4,7 @@ import com.youtube.maratonajava.ZZFthreads.dominio.Account;
 
 public class ThreadAccountMain01 implements Runnable {
 
-    private Account account = new Account();
+    private final Account account = new Account();
 
     public static void main(String[] args) {
 
@@ -25,16 +25,19 @@ public class ThreadAccountMain01 implements Runnable {
         }
     }
 
-    private void withdrawl(int amount) {
-        if(account.getBalance() >= amount) {
-            System.out.println(getThreadName() + " está indo sacar dinheiro");
-            account.withdrawl(amount);
-            System.out.println(getThreadName() + " completou o saque, valor atual da conta " +
-                    account.getBalance());
-        } else {
-            System.out.println("Sem dinheiro para " +
-                    getThreadName() + " efetuado o saque " +
-                    account.getBalance());
+    private synchronized void withdrawl(int amount) {
+        synchronized (account) {
+            System.out.println(Thread.currentThread().getName() + " com synchronized");
+            if (account.getBalance() >= amount) {
+                System.out.println(getThreadName() + " está indo sacar dinheiro");
+                account.withdrawl(amount);
+                System.out.println(getThreadName() + " completou o saque, valor atual da conta " +
+                        account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " +
+                        getThreadName() + " efetuado o saque " +
+                        account.getBalance());
+            }
         }
     }
 
